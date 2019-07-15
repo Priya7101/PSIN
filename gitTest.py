aaf0e49d43b278e630ffe7edfdb28c3addb320a3
priya singh <priyasingh7101@gmail.com>
2019-07-15 16:10:06 +0530

    if condition

--- gitTest.py
+++ gitTest.py
@@ -1,2 +1,8 @@
-print("hello Priya, welcome to git, committing changes")
+print("hello Priya, welcome to git!")
 print("New changes added")
+print("New code to be printed")
+
+if 5>3:
+    print("5 is greater than 3")
+else:
+    print("Simply text")
