9d21de4787f69f2191be2ef86ebad35aa4a88d41
priya singh <priyasingh7101@gmail.com>
2019-07-16 15:37:17 +0530

    code added to a new branch

--- hungry.py
+++ hungry.py
@@ -1,6 +1,10 @@
 print("HungryFood items-")
-hungry = "yes"
+hungry = "no"
+thirsty = "yes"
 if hungry=="yes":
     print("eat pizza")
     print("eat burger")
     print("eat fries")
+else:
+    if thirsty == "yes":
+        print("drink water")
