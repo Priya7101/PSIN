5e2aa3fba8046fbe2bc9e825c365594eab121905
priya singh <priyasingh7101@gmail.com>
2019-07-16 15:10:34 +0530

    python file code changed completely

--- gitTest.py
+++ gitTest.py
@@ -1,8 +1,6 @@
-print("hello Priya, welcome to git!")
-print("New changes added")
-print("New code to be printed")
-
-if 5>3:
-    print("5 is greater than 3")
-else:
-    print("Simply text")
+print("HungryFood items-")
+hungry = "yes"
+if hungry=="yes":
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
